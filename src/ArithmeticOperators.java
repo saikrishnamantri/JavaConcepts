3d05fd5365c7f0d3ce46d0430595c2b563398e71
saikrishnamantri <saikrishnamantri@icloud.com>
2026-09-12 17:07:46 -0400

    Added ArithmeticOperators and RelationalOperators

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 6;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b)); //Integer Division
+        System.out.println("a % b = " + (a % b)); //Remainder
+
+        double x = 20;
+        double y = 6;
+
+        System.out.println("x / y = " + (x / y)); //decimal division
+    }
+}
